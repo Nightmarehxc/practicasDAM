5d7854f4bf6ef19605ad36372134ab88cec7b1ed
dam <dcamarero2018@gmail.com>
2019-02-11 19:54:12 +0100

    animal

--- 2Trimestre/BBDD/Consultas/consultasSimplesMultiTablas.sql
+++ 2Trimestre/BBDD/Consultas/consultasSimplesMultiTablas.sql
@@ -41,9 +41,62 @@ SELECT c.`NombreCliente`,COUNT(p.`CodigoPedido`)
 	FROM clientes c, pedidos p
 		WHERE c.CodigoCliente = p.CodigoCliente
 		GROUP BY c.CodigoCliente;
+		
+		SELECT NombreCliente,COUNT(CodigoPedido)
+	FROM clientes INNER JOIN pedidos ON clientes.`CodigoCliente` = pedidos.`CodigoCliente`
+		GROUP BY CLIENTES.`CodigoCliente`;
 /*
+EJERCICIO 5
 	Obtener un listado con los nombres de los clientes
 	y el total pagado por cada uno de ellos.
 */
-SELECT 
-	FROM clientes c, productos p, 
+SELECT `NombreCliente`, SUM(cantidad) AS "Total pagado" 
+	FROM clientes LEFT JOIN pagos ON clientes.`CodigoCliente` = pagos.`CodigoCliente`
+		GROUP BY nombreCliente;
+
+/*
+eJERCICIO 6
+Obtener el nombre y apellidos de los empleados así como el nombre de cliente de aquellos empleados que representen a clientes que hayan efectuado algún pago mediante Paypal.
+*/
+SELECT  DISTINCT c.`NombreCliente`, e.`Nombre`,e.`Apellido1`,e.`Apellido2`, p.`FormaPago`
+	FROM empleados e, clientes c, pagos p, detallepedidos dp
+			WHERE p.`FormaPago` = "PayPal";	
+/*Ejercicio 7:
+Obtener cuántos empleados tiene cada oficina, mostrando el nombre de la ciudad donde está la oficina.
+*/
+SELECT 	o.`Ciudad`,COUNT(e.`CodigoEmpleado`)
+	FROM empleados e, oficinas o
+		WHERE e.`CodigoOficina` = o.`CodigoOficina`
+		GROUP BY o.ciudad;
+		
+		
+SELECT ciudad, COUNT(CodigoEmpleado)
+	FROM empleados INNER JOIN oficinas ON oficinas.`CodigoOficina` = empleados.`CodigoOficina`
+	GROUP BY ciudad;
+/*
+Ejercicio 8:
+	Obtener el nombre, apellido, oficina (ciudad) y cargo del empleado que no represente a ningún cliente.
+*/
+SELECT e.`Nombre`,e.`Apellido1`,e.`Apellido2`,e.`Puesto`,o.ciudad
+	FROM empleados e,oficinas o, clientes c
+		WHERE e.`CodigoEmpleado` != c.`CodigoEmpleadoRepVentas`;
+		
+/*
+Ejercicio 9: 
+Obtener un listado con los clientes y el identificador de transacción
+ de aquellos clientes que efectuaron algún pago en el año 2007.
+*/
+SELECT c.`NombreCliente`,p.`IDTransaccion`,p.`FechaPago`
+	FROM clientes c, pagos p
+		WHERE p.`FechaPago` LIKE "2007%";
+/*
+Ejercicio 10:
+Obtener los nombres de los clientes así como los nombres y apellidos de sus representantes de aquellos clientes que no han realizado pagos.
+*/
+/*select c.`NombreCliente`, c.`CodigoEmpleadoRepVentas`
+	from clientes c, pagos p
+		where c.*/
+		
+SELECT NombreCliente, Nombre, Apellido1, Apellido2
+	FROM clientes LEFT JOIN empleados ON empleados.`CodigoEmpleado`=clientes.`CodigoCliente`
+		GROUP BY nombre;
