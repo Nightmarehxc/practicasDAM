72271fa9a398a2977e0947ef83815323601cd497
dam <dcamarero2018@gmail.com>
2019-03-07 15:29:01 +0100

    Arrays3

---
+++ 2Trimestre/Programacion/Vectores3/src/Producto.java
@@ -0,0 +1,47 @@
+public class Producto
+{
+
+    int cod;
+    String name;
+    int stock;
+    double price;
+    public Producto()
+    {
+        int cod;
+        String name;
+        int stock;
+        double price;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
