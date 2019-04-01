e77fabd71230fe98572373308e1c01f9d693d99a
dam <dcamarero2018@gmail.com>
2019-04-01 15:42:12 +0200

    hundirFlota

---
+++ 3erTrimestre/Programacion/HundirFlota/src/Jugador.java
@@ -0,0 +1,8 @@
+public class Jugador
+{
+    int[][] lhd = new int[1][5] ;
+    int[][] acorazado = new int[1][4];
+    int[][] crucero = new int[1][3];
+    int[][] destructor =new int[1][2];
+    int[][] submarino= new int[2][1];
+}
---
+++ 3erTrimestre/Programacion/HundirFlota/src/Tablero.java
@@ -0,0 +1,5 @@
+public class Tablero
+{
+    String[][] table =  new String[10][10];
+    
+}
