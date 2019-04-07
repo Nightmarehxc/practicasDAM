bd21738f278cb13ad4489d6a7480c66f508c7cf5
Nightmare <nightmarehxc@gmail.com>
2019-04-07 17:36:31 +0200

    Update EjerciciosUT5_BBDDmod.sql
    actualizado el procedimiento

--- 3erTrimestre/BBDD/EjerciciosUT5_BBDDmod.sql
+++ 3erTrimestre/BBDD/EjerciciosUT5_BBDDmod.sql
@@ -170,22 +170,26 @@ INSERT INTO cuentas VALUES(2,10);
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS comprar_entrada;
-CREATE PROCEDURE comprar_entrada(IN nif VARCHAR(9), OUT error INT)
-SET error = 1;
-END$$
+CREATE PROCEDURE comprar_entrada(IN nif VARCHAR(9),IN cuenta INT UNSIGNED, IN id_cuenta INT UNSIGNED, IN id_butaca INT UNSIGNED,OUT error TINYINT UNSIGNED)
+BEGIN
+    DECLARE CONTINUE HANDLER FOR 1264,1062
+    BEGIN 
+    SET error = 1;
+    END;
 
 START TRANSACTION;
 SET error = 0;
 UPDATE cuentas SET saldo = saldo -5
 WHERE cuentas.id_cuenta = id_cuenta;
+
 INSERT INTO entradas VALUES (id_butaca, nif);
-if error = 0;
+
+if error = 0 THEN
 COMMIT;
 ELSE
 ROLLBACK;
 END IF;
-END
-
+END$$
 DELIMITER ;
 CALL comprar_entrada('12345678A', 1, 10, @error);
 CALL comprar_entrada('19876543B', 1, 11, @error);
