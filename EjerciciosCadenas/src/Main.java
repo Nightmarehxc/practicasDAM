f9aebd2faead948131f718b81fddbe0389fcf60b
Nightmare <nightmarehxc@gmail.com>
2019-04-16 13:50:35 +0200

    cadenas1

---
+++ EjerciciosCadenas/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Palabras juego1 = new Palabras;
+        
+
+        int espacios = juego1.palabras[0].
+
+    }
+}
