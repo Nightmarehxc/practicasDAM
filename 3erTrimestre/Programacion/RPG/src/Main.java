e63dddc2d6091ac0dc1775b11bd9af15ff515df8
Nightmare <nightmarehxc@gmail.com>
2019-05-13 13:24:30 +0200

    rpg

--- 3erTrimestre/Programacion/RPG/src/Main.java
+++ 3erTrimestre/Programacion/RPG/src/Main.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class Main {
 
-    static ArrayList<Pj> listaPlayers = new ArrayList<Pj>();
+    static ArrayList<> listaPlayers = new ArrayList<>();
     public static void main(String[] args)
     {
         listaPlayers.add(new Mago("Gnomo Ilusionista",4,6,50));
-
+        listaPlayers.get(0).
 
     }
 }
