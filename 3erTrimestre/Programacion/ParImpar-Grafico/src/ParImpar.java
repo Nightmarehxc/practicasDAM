b67daafa7fe74b635cff63b43b67439521413ddd
dam <dcamarero2018@gmail.com>
2019-05-22 16:55:20 +0200

    interfaz

---
+++ 3erTrimestre/Programacion/ParImpar-Grafico/src/ParImpar.java
@@ -0,0 +1,14 @@
+public class ParImpar
+{
+    static String Calcular(int a)
+    {
+        if(a%2==0){
+            System.out.println("Es par");
+            return "Es par";
+
+        }else{
+            System.out.println("Es impar");
+            return "Es impar";
+        }
+    }
+}
